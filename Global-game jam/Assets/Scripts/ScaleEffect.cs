f3abcda4009a20c1d7714ac5293a39f815eec63b
lethanov <lethanov@gmail.com>
2016-01-29 20:49:44 +0100

    Test mdr

---
+++ Global-game jam/Assets/Scripts/ScaleEffect.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleEffect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
