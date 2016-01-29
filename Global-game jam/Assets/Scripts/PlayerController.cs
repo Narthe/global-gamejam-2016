642f1b4b53a17ab1d9e0c06f566979bc5ddcd19e
Schmitt <clement.flance@gmail.com>
2016-01-29 22:58:47 +0100

    Add Player controller script + player & obstacles prefabs

--- Global-game jam/Assets/Scripts/PlayerController.cs
+++ Global-game jam/Assets/Scripts/PlayerController.cs
@@ -3,24 +3,32 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-	void Start ()
+    public float speed = 6f;
+    public Vector3 movement;
+    Rigidbody2D playerRigidbody;
+
+    void Awake()
     {
-	
-	}
-	
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+    
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, v, 0.0f);
+
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
 	void Update ()
     {
-	    if(Input.GetKey(KeyCode.UpArrow))
-        {
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-        }
     }
 }
