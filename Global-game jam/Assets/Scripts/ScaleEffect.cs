f91f22c211b5c057a4f5fb700b674dd5e916d2be
lethanov <lethanov@gmail.com>
2016-01-29 21:50:57 +0100

    Scale Effect Script

--- Global-game jam/Assets/Scripts/ScaleEffect.cs
+++ Global-game jam/Assets/Scripts/ScaleEffect.cs
@@ -2,14 +2,29 @@
 using System.Collections;
 
 public class ScaleEffect : MonoBehaviour {
+	
+	public bool effect; // true = out, false = in
+	public float factorValue;
 
-	// Use this for initialization
-	void Start () {
-	
+	private float scaleValue;
+
+	void Start () {	
+		scaleValue = transform.localScale.x;
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+		//Fade out ?
+		if(effect){
+			if(scaleValue > 0){
+				scaleValue -= factorValue;
+			} else {
+				scaleValue = 0;
+			}
+		} else {
+			scaleValue += factorValue;
+		}
+
+		//Gfx update
+		transform.localScale = new Vector2(scaleValue, scaleValue);
 	}
 }
