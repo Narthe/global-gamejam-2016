08a3304ae7e6f54f9664b7610511ffeb89277af2
lethanov <lethanov@gmail.com>
2016-01-30 21:37:46 +0100

    Test Sounds

--- Global-game jam/Assets/Scripts/MenuEffect.cs
+++ Global-game jam/Assets/Scripts/MenuEffect.cs
@@ -4,5 +4,6 @@ using System.Collections;
 public class MenuEffect : MonoBehaviour {
 	public void CreateEffect(){
 		Instantiate(Resources.Load("MenuSelectEffect"), transform.position, Quaternion.identity);
+		Instantiate(Resources.Load("MenuSelectSound"));
 	}
 }
