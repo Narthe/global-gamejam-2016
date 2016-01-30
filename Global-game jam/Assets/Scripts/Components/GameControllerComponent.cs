2e92c45aa2e765f3241b5c33b738d0f1a463de83
kevin <kevin.bollini@gmail.com>
2016-01-30 03:01:50 +0100

    Metronome et debut d'ui

---
+++ Global-game jam/Assets/Scripts/Components/GameControllerComponent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Components
+{
+    public class GameControllerComponent : MonoBehaviour
+    {
+        private float _rateInSec;
+        public int BPMRate = 110;
+        public float Curr;
+
+        public static GameControllerComponent Instance;
+
+        // Use this for initialization
+        void Start ()
+        {
+            Instance = this;
+        }
+	
+        // Update is called once per frame
+        void Update () {
+            _rateInSec = (BPMRate / 60);
+            Curr = Time.time % _rateInSec / _rateInSec;
+        }
+    }
+}
---
+++ Global-game jam/Assets/Scripts/Components/UI/MetronomeComponent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Components.UI
+{
+    public class MetronomeComponent : MonoBehaviour
+    {
+        public Image Target;
+        private RectTransform _rect;
+
+        // Use this for initialization
+        void Start () {
+	
+        }
+	
+        // Update is called once per frame
+        void Update ()
+        {
+            _rect = Target.rectTransform;
+            _rect.offsetMin = new Vector2(Mathf.Lerp(0, 1920-4,GameControllerComponent.Instance.Curr),0);
+            _rect.offsetMax = new Vector2(Mathf.Lerp(4, 1920, GameControllerComponent.Instance.Curr), 200);
+        }
+    }
+}
