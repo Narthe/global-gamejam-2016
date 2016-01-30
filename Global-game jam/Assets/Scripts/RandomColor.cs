0e3b07dd88887046a659fd348ba36017f5e2f9c9
lethanov <lethanov@gmail.com>
2016-01-30 16:31:46 +0100

    Menu Transition Fix + Assets

---
+++ Global-game jam/Assets/Scripts/RandomColor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<SpriteRenderer>().color = new Color(Random.Range(0.1f, 0.2f), Random.Range(0.8f, 1f), 0, 1f);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
