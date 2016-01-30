8320acc5d92f3b92b453673bebb70ea2b0e50aac
Schmitt <clement.flance@gmail.com>
2016-01-30 13:29:43 +0100

    Add Path and Player controller script

---
+++ Global-game jam/Assets/Scripts/Components/PathControllerComponent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathControllerComponent : MonoBehaviour
+{    
+         
+	void Start ()
+    {
+	    for(int i = 0; i < transform.GetComponent<iTweenPath>().nodeCount; i++)
+        {
+            GameObject wayPointGameobject = new GameObject();
+            
+        }
+	}
+	
+	void Update ()
+    {
+	
+	}
+}
