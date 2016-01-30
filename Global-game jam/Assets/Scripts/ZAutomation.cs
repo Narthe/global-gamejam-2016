503fe81cd381b69c6f68ca1b48a35607fb052457
lethanov <lethanov@gmail.com>
2016-01-30 23:25:48 +0100

    Bordel 2 : mdr

---
+++ Global-game jam/Assets/Scripts/ZAutomation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZAutomation : MonoBehaviour {
+	
+	void Start () {
+		foreach(GameObject victim in GameObject.FindGameObjectsWithTag("ZAutomated")){
+			victim.GetComponent<SpriteRenderer>().sortingOrder = (int)(victim.transform.position.y * -100)	;
+		}
+	}
+	
+
+}
